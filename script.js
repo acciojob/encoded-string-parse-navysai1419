ec23556696539824f9ef1393bd39e43f727d3c41
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-26 15:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const parseCode = (str) => {
-  // Split the input string using "000" as the separator
-   const [firstName, lastName, id] = encodedString.split('000');
+ const parts = str.split("000");
 
-  // Return an object with firstName, lastName, and id properties
-  return { firstName, lastName, id };
-};  
- 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+  // Remove any leading or trailing empty strings caused by "000"
+  const cleanParts = parts.filter(part => part !== "");
+
+  // Extract firstName, lastName, and id from the cleanParts array
+  const firstName = cleanParts[0] || "";
+  const lastName = cleanParts[1] || "";
+  const id = cleanParts[2] || "";
+
+  // Return an object with the extracted values
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  }; 
+};   
+
+// Prompt the user to enter a string
+const userInput = prompt("Enter str:");
+
+// Call the parseCode function with user input
+const result = parseCode(userInput);
+
+// Display the result using alert
+alert(JSON.stringify(result));
