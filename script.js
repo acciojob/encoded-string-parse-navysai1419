e572b94876dfd27bca8c4da8f0b1a0745525f17b
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-26 15:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const parseCode = (str) => {
   // Split the input string using "000" as the separator
-  const parts = str.split("0");
+   const [firstName, lastName, id] = encodedString.split('000');
 
-  // Extract the firstName, lastName, and id from the parts array
-  const firstName = parts[0];
-  const lastName = parts[1]; 
-  const id = parts[2];
-
-  // Create and return an object with the extracted values
-  const result = {
-    firstName,
-    lastName,
-    id 
-  };
-
-  console.log(result); // Log the result to the console for debugging
-
-  return result;
-};
+  // Return an object with firstName, lastName, and id properties
+  return { firstName, lastName, id };
+};  
  
 // Do not change the code below
 const str = prompt("Enter str: ");
